d6bf72d41bbfe5805a78b504576aa3d3b42aacd3
joanna broad <joanna.broad93@gmail.com>
2016-09-07 10:12:27 +0100

    when

--- program3.rb
+++ program3.rb
@@ -1,12 +1,15 @@
 def choose
-	puts "Do you like programming? Yes or No"
+	puts "Do you like programming? Yes , No or Maybe please"
 	choice = gets.chomp
-	if (choice.downcase == "yes")
+	case choice.downcase
+	when "yes"
 		puts "That\'s great!"
-	elsif (choice.downcase == "no")
-		puts "That\'s to bad!"
+	when "no"
+		puts "That\'s too bad!"
+	when "maybe"
+		puts "Glad you are giving it a chance!"
 	else
-		puts "That wasn\'t Yes or No"
+		puts "I don\'t know what that means."
 	end
 end
 
