2faa769b102580210831333c72443fb0b3279ea7
joanna broad <joanna.broad93@gmail.com>
2016-09-07 11:58:18 +0100

    program4

--- program4.rb
+++ program4.rb
@@ -1,6 +1,10 @@
 number  = 0
 
-while (number <= 10) do
-		p "the numer is now #{number}"
-		number += 1
+(0..10).each do |n|
+	p "The new number is #{n}"
 end
+
+# while (number <= 10) do
+# 		p "the numer is now #{number}"
+# 		number += 1
+# end
